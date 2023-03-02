328a494d3dbb9f4e7aa0ed007a788c4c0aaeb262
JuanDiegoAlaPer <juandiegoap2000@gmail.com>
2023-03-02 16:45:50 -0500

    Punto 20 alcanzado

---
+++ proy_client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1> Profe, póngame 5 </h1>
+    </div>
+  );
+}
+
+export default App;
